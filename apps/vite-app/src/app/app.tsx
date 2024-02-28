66e9d621e79764d88c87cbff23a5d82297c9647e
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2024-02-28 13:39:06 +0100

    feat: add vite-app

---
+++ apps/vite-app/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Section } from '@cmpsr/components';
+import { ExampleComponent } from '@ui';
+
+export function App() {
+  return (
+    <Section>
+      <ExampleComponent />
+    </Section>
+  );
+}
+
+export default App;
---
+++ apps/vite-app/src/main.tsx
@@ -0,0 +1,17 @@
+import { ComposerProvider } from '@cmpsr/components';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './app/app';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <ComposerProvider>
+        <App />
+      </ComposerProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
